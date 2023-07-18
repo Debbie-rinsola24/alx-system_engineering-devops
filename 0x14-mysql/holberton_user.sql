e29fe1454a5af4adb1f24b87c5a501c29b2a89aa
Debbie-rinsola24 <rinsolatiti@gmail.com>
2023-07-18 15:16:35 -0700

    mysql

--- 0x14-mysql/holberton_user.sql
+++
@@ -1,4 +0,0 @@
--- making a user for holberton
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-SHOW GRANTS FOR 'holberton_user'@'localhost';
